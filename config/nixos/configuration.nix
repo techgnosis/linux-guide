c02e7405ef395fcb846b28da4cc3936949e2fbd9
James Musselwhite <james.musselwhite@gmail.com>
2024-10-09 06:00:42 +0000

    hyprland is good

--- config/nixos/configuration.nix
+++ config/nixos/configuration.nix
@@ -28,15 +28,19 @@
     vulkan-tools
     vscodium
     firefox
+    hyprland
+    kitty
+    fuzzel
   ];
 
   fonts.packages = with pkgs; [
     (nerdfonts.override { fonts = [ "DroidSansMono" ]; })
   ];
 
-  services.xserver.enable = true;
-  services.xserver.desktopManager.xfce.enable = true;
-  services.xserver.displayManager.lightdm.enable = true;
+  #services.xserver.enable = true;
+  #services.xserver.desktopManager.xfce.enable = true;
+  #services.xserver.displayManager.lightdm.enable = true;
+  programs.hyprland.enable = true;
 
   # DO NOT CHANGE UNDER ANY CIRCUMSTANCE
   system.stateVersion = "24.05"; # Did you read the comment?
